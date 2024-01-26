95841d7aefb3ce6634866204c383a2ac53b4db1c
Daniel <danielrafaelrr@hotmail.com>
2024-01-25 21:53:49 -0600

    Initial Commit

---
+++ CalculadoraPropinas/CalculadoraPropinasApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculadoraPropinasApp.swift
+//  CalculadoraPropinas
+//
+//  Created by Daniel Rodriguez on 25/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CalculadoraPropinasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CalculadoraPropinas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CalculadoraPropinas
+//
+//  Created by Daniel Rodriguez on 25/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
